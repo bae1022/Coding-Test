c74fe86594f0f0ae2385a11f974f6d2c3f47821f
bae1022 <shbk0423@naver.com>
2021-09-27 16:39:11 +0900

    Heap_최대_힙

---
+++ Baekjoon/#11279.py
@@ -0,0 +1,19 @@
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+
+heap = []
+for i in range(n):
+    x = int(sys.stdin.readline())
+
+    if x > 0:
+        heapq.heappush(heap, -x)
+
+    else:
+        if len(heap) == 0:
+            print(0)
+
+        else:
+            num = heapq.heappop(heap)
+            print(-num)
