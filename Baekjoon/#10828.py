6cb83cafd1659c82ee34f97e32441444caf7e788
bae1022 <shbk0423@naver.com>
2021-09-29 11:22:31 +0900

    Stack_스택

---
+++ Baekjoon/#10828.py
@@ -0,0 +1,34 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+stack = []
+for _ in range(n):
+    m = str(sys.stdin.readline())
+
+    a = m.split()
+
+    if a[0] == 'push':
+        stack.append(a[1])
+
+    elif a[0] == 'top':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+
+    elif a[0] == 'size':
+        print(len(stack))
+
+    elif a[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+
+        else:
+            print(0)
+
+    elif a[0] == 'pop':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
