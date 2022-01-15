74ddb252bc9e432e3e7d7c8e9b69632af9935dcb
bae1022 <shbk0423@naver.com>
2022-01-15 22:10:53 +0900

    단어뒤집기2

---
+++ Baekjoon/#17413.py
@@ -0,0 +1,28 @@
+s = list(input())
+
+i = 0
+start = 0
+while i < len(s):
+    if s[i] == '<':
+        i += 1
+
+        while s[i] != '>':
+            i += 1
+
+        i += 1
+
+    elif s[i].isalnum():
+        start = i
+
+        while i < len(s) and s[i].isalnum():
+            i += 1
+
+        tmp = s[start:i]
+        tmp.reverse()
+
+        s[start:i] = tmp
+
+    else:
+        i += 1
+
+print(''.join(s))
