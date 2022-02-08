89ea6f36a31725507ae9116f22f2ce460d54ae60
bae1022 <shbk0423@naver.com>
2022-02-08 18:57:30 +0900

    피카츄

---
+++ Baekjoon/#14405.py
@@ -0,0 +1,15 @@
+s = input()
+
+if len(s) >= 2:
+    s = s.replace('pi', ' ')
+    s = s.replace('ka', ' ')
+    s = s.replace('chu', ' ')
+
+    if len(s.strip()) == 0:
+        print('YES')
+
+    else:
+        print('NO')
+
+else:
+    print('NO')
